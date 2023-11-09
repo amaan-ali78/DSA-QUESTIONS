b5197f221039529cf50f6c370b054aa0741c44b4
Amaan Ali <amaanali.aa711@gmail.com>
2023-11-09 23:37:33 +0530

    Optimization

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -22,7 +22,7 @@ public class ArrayReverse
         System.out.print("Enter the size of your Array : ");
         n = sc.nextInt();
         int[] arr = new int[n];
-        System.out.println("Enter the element in your Array");
+        System.out.print("Enter the element in your Array : ");
         for (int i = 0; i < arr.length; i++) 
         {
             arr[i] = sc.nextInt();
