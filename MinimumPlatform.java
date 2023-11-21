86caafa547f76c1ee359b1303aba29961d88862e
Amaan Ali <amaanali.aa711@gmail.com>
2023-11-21 17:33:53 +0530

    Minimum Platform

--- MinimumPlatform.java
+++ MinimumPlatform.java
@@ -1,19 +1,25 @@
-public class MinimumPlatform {
-    static void countPlatform(int arr1[], int arr2[], int n) {
+public class MinimumPlatform 
+{
+    static void countPlatform(int arr1[], int arr2[], int n) 
+    {
         int count = 0;
-        for (int i = 0; i < arr1.length - 1; i++) {
+        for (int i = 0; i < n - 1; i++) 
+        {
             if (arr1[i] + (arr2[i] - arr1[i]) >= arr1[i + 1])
                 count++;
         }
-        if (count == 0) {
+        if (count == 0) 
+        {
             count = 1;
         }
         System.out.println("No of  platform required is : " + count);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int arr1[] = { 900, 940, 950, 1100, 1500, 1800 };
         int arr2[] = { 910, 1200, 1120, 1130, 1900, 2000 };
         countPlatform(arr1, arr2, arr1.length);
+        countPlatform(arr1, arr2, 2); 
     }
 }
